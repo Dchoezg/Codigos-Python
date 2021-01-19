ddfff8fde17b10d8569c178acd5839d89488593c
Dchoezg <76454809+Dchoezg@users.noreply.github.com>
2021-01-18 21:37:19 -0500

    4TA CLASE 18_01_2021

---
+++ Input.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 18 15:12:21 2021
+
+@author: RYZEN
+"""
+#CLASE 18/01/2021
+#INGRESO DE VALORES
+leer=input('Ingrese su nombre:')
+print('hola!',leer)
